2020542664a5ddbfeac2b38dc7bc8260ecb9bc2a
unknown <gilson.costa@dce.ufpb.br>
2013-01-28 14:05:06 -0300

    teste

---
+++ Teste/TesteJogado.java
@@ -0,0 +1,46 @@
+package Teste;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import Tabuleiro.*;
+
+public class TesteJogado {
+
+	private Jogado jogado;
+	
+	@Before
+	public void setUp(){
+		this.jogado = new Jogado("gilson");
+	}
+	@After
+	public void tearDown(){
+		
+	}
+	@Test
+	public void qualJogadoTest(){
+		if(this.jogado.getQualogado()){
+			fail("não deveria ter entrado");
+		}
+		this.jogado.setQualJogado(true);
+		assertTrue(this.jogado.getQualogado());
+	}
+	@Test
+	public void nomeDoJogadoTest(){
+		if(!this.jogado.getNome().equals("gilson")){
+			fail("não deveria ter entrado");
+		}
+		this.jogado.setNome("italo");
+		assertEquals("italo",this.jogado.getNome());
+	}
+	@Test
+	public void pontosDoJogadoTest(){
+		if(this.jogado.getPonto() != 12){
+			fail("não deveria ter entrado");
+		}
+		this.jogado.setPonto();
+		assertEquals(11,this.jogado.getPonto());
+		this.jogado.novaPartida();
+		assertEquals(12,this.jogado.getPonto());
+	}
+}
