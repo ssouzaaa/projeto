0d2e2e841bf189add495d262e5fc282c3097e2cd
unknown <gilson.costa@dce.ufpb.br>
2013-01-30 14:11:34 -0300

    teste italo

---
+++ TestItalo/BotaoTest.java
@@ -0,0 +1,40 @@
+package Teste;
+
+import static org.junit.Assert.*;
+
+import javax.swing.ImageIcon;
+
+import org.junit.*;
+
+import Tabuleiro.Botao;
+
+public class BotaoTest {
+
+	
+	@Test
+	public void novaPartidaTest(){
+		Botao b = new Botao(0,0);
+		assertFalse(b.getPordi());
+		assertFalse(b.getEhDama());
+		b.setPordi(true);
+		b.setEhDama(true);
+		assertTrue(b.getPordi());
+		assertTrue(b.getEhDama());
+		b.novaPartida(new ImageIcon(), 1);
+		assertFalse(b.getPordi());
+		assertFalse(b.getEhDama());
+		assertEquals(1,b.getTipoBotao());
+		b.setTipoBotao(2);
+		assertEquals(2,b.getTipoBotao());
+	}
+	@Test
+	public void testPosicao(){
+		Botao b = new Botao(0,0);
+		int[] p = b.posicao();
+		if(!(p[0]==0 & p[1]==0)){
+			fail("Não deveria ter entrado");
+		}
+	}
+
+}
+
