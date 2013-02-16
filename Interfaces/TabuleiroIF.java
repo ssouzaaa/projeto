084541343269342540345be69b4b744abb18f6f4
unknown <gilson.costa@dce.ufpb.br>
2013-02-16 20:47:46 -0300

    interfaces

--- Interfaces/TabuleiroIF.java
+++ Interfaces/TabuleiroIF.java
@@ -1,14 +1,30 @@
 package Interfaces;
 
-import Tabuleiro.Jogado;
-
 public interface TabuleiroIF {
 
-	void novaPartida(Jogado jogado1,Jogado jogado2);
+	/**
+	 * metodo para inicia um novo jogo, restaurando
+	 * assim o tabuleiro e as peças as suas formas
+	 * inicias.
+	 */
+	void novaPartida();
 	
+	/**
+	 * metodo que permite apenas as peças azuis serem 
+	 * clicadas no jogo.
+	 */
 	void jogadaAzul();
 	
+	/**
+	 * metodo que permite apenas as peças vermelhas a
+	 * serem clicadas no jogo.
+	 */
 	void jogadaVermelha();
 	
+	/**
+	 * metodo que proibi tudo com a finalidade de 
+	 * quando o jogado termia sua partida não seja
+	 * deixada nenhua peça que possa ser clicada.
+	 */
 	void tudoProibido();
 }
